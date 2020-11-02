a2d0fc9e221a99bfea9bbc78c4e52c9f66767ea4
Christophe <pro.chrisrln@gmail.com>
2020-11-02 14:35:52 +0100

    Chris : Init my Magic Calculator App

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Christophe REALINI on 02/11/2020.
+//  Copyright © 2020 christophe-realini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
